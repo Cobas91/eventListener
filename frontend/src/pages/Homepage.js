be7a9fa24e593d0c078dbd32a2a6c5e717fc8907
Stephan Gärtner <stephan.gaertner72@outlook.de>
2021-10-29 14:50:57 +0200

    Add Homepage Style with Dummy Text

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,43 @@
 import * as React from 'react'
+import styled from 'styled-components'
+import { Card, Typography } from '@mui/material'
 
 export default function Homepage() {
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+    <HomepageContainer>
+      <Title variant="h4">Zapliance EventListener</Title>
+      <StyledCard>
+        <Typography>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </Typography>
+      </StyledCard>
+    </HomepageContainer>
   )
 }
+const StyledCard = styled(Card)`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+  padding: 25px;
+  margin: 100px;
+`
+
+const Title = styled(Typography)`
+  margin: 10px;
+`
+
+const HomepageContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
