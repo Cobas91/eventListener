dfd1799717ddb492da76b3e98776dfd9eeba8d7e
Stephan Gärtner <stephan.gaertner72@outlook.de>
2021-10-29 16:12:06 +0200

    Merge fixes

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,6 +1,5 @@
 import { Redirect, Route } from 'react-router-dom'
 
 export default function PrivateRoute(props, loggedIn) {
-  //TODO Add AuthContext and Check on loggedIn
   return loggedIn ? <Route {...props} /> : <Redirect to={'/login'} />
 }
