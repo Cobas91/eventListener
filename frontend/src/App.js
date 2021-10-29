c2dc79197d0a9a5206dfc3e84f4ec7d8027bd426
Stephan Gärtner <stephan.gaertner72@outlook.de>
2021-10-29 12:45:18 +0200

    Adding Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
+import Navigation from './components/Navigation'
+import { Route, Switch } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          EventListener
-        </p>
+        <Navigation />
       </header>
+      <Switch>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
