905e7d36444c46607df96a521bf846d7d0f8e82a
Stephan Gärtner <stephan.gaertner72@outlook.de>
2021-11-08 17:08:32 +0100

    Swagger init

---
+++ backend/src/main/java/eventlistener/config/ReactRoutingForward.java
@@ -0,0 +1,12 @@
+package eventlistener.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactRoutingForward {
+    @RequestMapping("/**/{[path:[^\\.]*}")
+    public String forwardToOurUrl(){
+        return "forward:/";
+    }
+}
