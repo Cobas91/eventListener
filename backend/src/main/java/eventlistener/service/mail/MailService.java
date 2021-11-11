d8039925f8d1d8f38503eb35da886ffd9440b268
Stephan Gärtner <stephan.gaertner72@outlook.de>
2021-11-11 14:53:02 +0100

    Modify with E-Mail Template as HTML File

--- backend/src/main/java/eventlistener/service/mail/MailService.java
+++ backend/src/main/java/eventlistener/service/mail/MailService.java
@@ -2,37 +2,57 @@ package eventlistener.service.mail;
 
 import eventlistener.model.event.EventContentDTO;
 import eventlistener.model.notificationuser.NotificationUser;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
 
+import org.springframework.mail.MailSendException;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
 import java.util.List;
 
 @Service
 public class MailService {
 
+    private final TemplateEngine templateEngine;
     private final JavaMailSender emailSender;
 
-    public MailService(JavaMailSender emailSender) {
+    public MailService(JavaMailSender emailSender, TemplateEngine templateEngine) {
         this.emailSender = emailSender;
+        this.templateEngine = templateEngine;
     }
 
-    public void handleMailAction(EventContentDTO eventDetails, List<NotificationUser> users) throws MailException {
+    public void handleMailAction(EventContentDTO eventDetails, List<NotificationUser> users){
         for (NotificationUser user : users) {
+            String content = templateEngine.process("event", setMailContext(user, eventDetails));
             String subject = eventDetails.getEnvironmentName() + " " + " Notification";
-            sendMail(user.getEmail(), subject, eventDetails.getContent());
+            try{
+                sendMail(subject, content, user);
+            }catch (MessagingException e) {
+                throw new MailSendException("Failure sending Mail for Event with ID: "+eventDetails.getId(), e);
+            }
         }
 
     }
+    private Context setMailContext (NotificationUser user, EventContentDTO event){
+        Context context = new Context();
+        context.setVariable("user", user);
+        context.setVariable("event", event);
+        return context;
+    }
 
-    private void sendMail(String to, String subject, String body){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("events@zapliance.com");
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        emailSender.send(message);
+    private void sendMail(String subject, String content, NotificationUser user) throws MessagingException {
+        javax.mail.internet.MimeMessage mimeMessage = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+        helper.setSubject(subject);
+        helper.setFrom("events@zapliance.com");
+        helper.setText(content, true);
+        helper.setTo(user.getEmail());
+        emailSender.send(mimeMessage);
+
     }
 
 }
