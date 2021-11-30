a84928ead75117f9c0c71db7f0ba71aa6099f75f
Stephan Gärtner <stephan.gaertner72@outlook.de>
2021-11-30 12:33:44 +0100

    add copyright

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2021. Zapliance GmbH All Rights Reserved.
+ * You may use, distribute and modify this code under the terms of the Zapliance license,
+ * which unfortunately won't be written for another century.
+ * You should have received a copy of the Zapliance license with
+ * this file. If not, please visit : https://zapliance.com
+ */
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
@@ -16,7 +24,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
